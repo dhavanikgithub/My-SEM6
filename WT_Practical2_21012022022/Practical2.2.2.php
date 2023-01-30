819ada0c043f4641c53ea45664bca371a6e376f1
dhavanikgithub <110646988+dhavanikgithub@users.noreply.github.com>
2023-01-30 17:33:10 +0530

    Delete WT_Practical2_21012022022 directory

--- WT_Practical2_21012022022/Practical2.2.2.php
+++
@@ -1,9 +0,0 @@
-<!-- Embedding HTML markups in PHP code -->
-<?php
-    echo "<html><body>";
-    for($i=0;$i<5;$i++)
-    {
-        echo "<div><h1>Dhavanik</h1></div>";
-    }
-    echo "</body></html>";
-?>
--- WT_Practical2_21012022022/Practical2.9.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $num = readline("Enter Number: ");
-    for($i=1;$i<=10;$i++)
-    {
-        echo $num." * ".$i.": ".$num * $i."\n";
-    }
-?>
